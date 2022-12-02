2eeda289bf7f981edd3b1588644e1deddad0a9b8
JoshuaMeza <jmeza@cubicode.mx>
2022-12-02 13:18:09 -0600

    Planet movement

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,7 @@
 // Fields
+const LEFT = 0;
+const RIGHT = 1;
+const SLEEP_TIME = 80;
 const CONTAINER = $("#world-container");
 const PREV_BTN = $("#previous > i");
 const NEXT_BTN = $("#next > i");
@@ -7,25 +10,36 @@ const JM = $("#img-jm");
 const JG = $("#img-jg");
 const RG = $("#img-rg");
 
-// Helper
-function hideAllWorldContainerContents() {
+// Helpers
+function resetWorldContainerContents() {
 	TITLE_CONTAINER.addClass("d-none");
 	JM.addClass("d-none");
 	JG.addClass("d-none");
 	RG.addClass("d-none");
+	JM.addClass("appear-jm");
+	JG.addClass("appear-jg");
+	RG.addClass("appear-rg");
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getFrameClass(id) {
+	var s = "00" + id;
+	return "world-frame-" + s.substring(s.length - 2);
 }
 
 // Classes
 class TransitionManager {
 	constructor(initialState) {
 		this.state = initialState;
-		this.state.executeAction();
 	}
 
 	previousState() {
 		if (this.state.previous != null) {
+			this.state.executeProcess(LEFT);
 			this.state = this.state.previous;
-			this.state.executeAction();
 		} else {
 			alert("Previous step is undefined!");
 		}
@@ -33,8 +47,8 @@ class TransitionManager {
 
 	nextState() {
 		if (this.state.next != null) {
+			this.state.executeProcess(RIGHT);
 			this.state = this.state.next;
-			this.state.executeAction();
 		} else {
 			alert("Next step is undefined!");
 		}
@@ -42,10 +56,21 @@ class TransitionManager {
 }
 
 class TransitionState {
-	constructor(action) {
+	constructor(action, leftAnimation, rightAnimation) {
 		this.action = action;
+		this.leftAnimation = leftAnimation;
+		this.rightAnimation = rightAnimation;
 		this.previous = null;
 		this.next = null;
+	}
+
+	executeProcess(direction) {
+		resetWorldContainerContents();
+		if (direction == LEFT) {
+			this.leftAnimation(this.previous);
+		} else if (direction == RIGHT) {
+			this.rightAnimation(this.next);
+		}
 	}
 
 	executeAction() {
@@ -54,8 +79,6 @@ class TransitionState {
 	}
 
     defaultAction() {
-		hideAllWorldContainerContents();
-		CONTAINER.removeClass();
 		if (this.previous == null) {
 			PREV_BTN.addClass("d-none");
 		} else {
@@ -71,33 +94,97 @@ class TransitionState {
 
 // States
 var state0 = new TransitionState(() => {
-	CONTAINER.addClass("world-frame-01");
-	
+	// FRAME 1
 	TITLE_CONTAINER.removeClass("d-none");
 	JM.removeClass("d-none");
 	JG.removeClass("d-none");
 	RG.removeClass("d-none");
 
-	setTimeout(function () {
-		TITLE_CONTAINER.removeClass("appear-title");
-		JM.removeClass("appear-jm");
-		JG.removeClass("appear-jg");
-		RG.removeClass("appear-rg");
-	}, 2000)
+	setTimeout(() => {JM.removeClass("appear-jm")}, 1700);
+	setTimeout(() => {JG.removeClass("appear-jg")}, 1600);
+	setTimeout(() => {RG.removeClass("appear-rg")}, 1800);
+}, async (state) => {
+}, async (state) => {
+	for (let i = 2; i <= 4; i++) {
+		await sleep(SLEEP_TIME);
+		CONTAINER.removeClass();
+		CONTAINER.addClass(getFrameClass(i));
+	}
+
+	state.executeAction();
 });
+
 var state1 = new TransitionState(() => {
-	CONTAINER.addClass("world-frame-02");
+	// FRAME 4
+	console.log("action STATE 1");
+}, async (state) => {
+	for (let i = 3; i >= 1; i--) {
+		await sleep(SLEEP_TIME);
+		CONTAINER.removeClass();
+		CONTAINER.addClass(getFrameClass(i));
+	}
+
+	state.executeAction();
+}, async (state) => {
+	for (let i = 5; i <= 12; i++) {
+		await sleep(SLEEP_TIME);
+		CONTAINER.removeClass();
+		CONTAINER.addClass(getFrameClass(i));
+	}
+
+	state.executeAction();
+});
+
+var state2 = new TransitionState(() => {
+	// FRAME 12
+	console.log("action STATE 2");
+}, async (state) => {
+	for (let i = 11; i >= 4; i--) {
+		await sleep(SLEEP_TIME);
+		CONTAINER.removeClass();
+		CONTAINER.addClass(getFrameClass(i));
+	}
+
+	state.executeAction();
+}, async (state) => {
+	for (let i = 13; i <= 17; i++) {
+		await sleep(SLEEP_TIME);
+		CONTAINER.removeClass();
+		CONTAINER.addClass(getFrameClass(i));
+	}
+	
+	state.executeAction();
+});
+
+var state3 = new TransitionState(() => {
+	// FRAME 17
+	console.log("action STATE 3");
+}, async (state) => {
+	for (let i = 16; i >= 12; i--) {
+		await sleep(SLEEP_TIME);
+		CONTAINER.removeClass();
+		CONTAINER.addClass(getFrameClass(i));
+	}
+	
+	state.executeAction();
+}, async (state) => {
 });
 
 // Assign states
-state0.previous = state1;
-state1.next = state0;
+state0.next = state1;
+state1.previous = state0;
+state1.next = state2;
+state2.previous = state1;
+state2.next = state3;
+state3.previous = state2;
 
-// Init manager
+// Init
 var manager = new TransitionManager(state0);
+CONTAINER.addClass("world-frame-01");
 
-// Enable buttons
 $(window).on("load", function () {
+	state0.executeAction();
+	
 	PREV_BTN.click(function () {
 		manager.previousState();
 	});
